8c1a816602910591d38981ad0e20508ee08c8593
Yuvaraj <yuvarajpuggi19@gmail.com>
2025-09-14 12:41:38 +0000

    ⚠ Blocked cross-origin request from 9003-firebase-studio-1748097386089.c

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  devIndicators: {
+    allowedDevOrigins: [
+      'https://9003-firebase-studio-1748097386089.cluster-zumahodzirciuujpqvsniawo3o.cloudworkstations.dev',
+    ],
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
